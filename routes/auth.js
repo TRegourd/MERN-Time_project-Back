44ceec7c59b10c768fadf6b1c9e99d84367f8079
Thomas Regourd <tom.reg@hotmail.fr>
2022-06-07 12:17:00 +0200

    add forgot route, controller and mail

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,10 @@ router.post("/login", Auth.login);
 /* Signin */
 router.post("/signin", Auth.signin);
 
+/* Forgot password */
+router.post("/forgot", Auth.forgot);
+
+/* Reset password */
+router.put(`/reset/:id`, Auth.reset);
+
 module.exports = router;
